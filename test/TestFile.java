c27870afc2f31dbffce6c421a494f78f99dcccd7
Cesar Yeep <ceyeep@gmail.com>
2013-01-04 22:55:05 -0700

    Resolving issue #21 - Creating a general purpose test file with most important features of CJC. (still missing some features) - This is a compilable CJC file.

--- test/TestFile.java
+++ test/TestFile.java
@@ -9,30 +9,138 @@
  * Contributors:
  *     Cesar Yeep - initial API and implementation
  ******************************************************************************/
+/*
+ * This file contains annotation examples of supported CleanJava features.
+ * 
+ * Notes:
+ *  This program was tested with zero error compilation using a CleanJava Checker.
+ *  Some of the methods don't have a complete implementation; this
+ *  program is focused on the documentation of CleanJava annotations.
+ */
 package test;
 
-/** This file includes the main features supported by CJC. */
+/** This file contains annotation examples of supported CleanJava features. */
 public class TestFile{
+	@SuppressWarnings("unused")
+	private static int zero;
+	@SuppressWarnings("unused")
+	private static boolean flag;
+	@SuppressWarnings("unused")
+	private int x = 0, y = 0, z = 0, sum = 0, max = 0;
+	
+	//Class body declarations (entry CJ annotation point)
+	
+	//Static initializer
+	//@ [ zero := 0 ]
+	static{
+		zero = 0;
+	}
+	
+	//Instance initializer
+	//@ [ x := 3 ]
+	{
+		x = 5;
+	}
+	
+	//Constructor declaration
+	//@ [ x := 3]
+	public TestFile (){
+		x = 3;
+	}
+
+	//Method declaration	 
+	//@ [ flag := true ]
+	public static void main(String[] args) {
+		flag = true;
+	}
+	
+	
 	//CleanJava annotations
-		//Single line annotation
-		//Multi line annotation
-		//Commment inside annotation
+	
+	//Single-line annotation
+	//@ [ x := 3]
+	public void singleLineAnnotation(){}
+	
+	//Multi-line annotation
+	/*@ 
+	   [ x == 0 -> y := 0
+	    \else x > 0 -> y := 1 
+	    \else x < 0 -> y := -1 ]
+	 @*/
+	public void multiLineAnnotation(){}
+	
+	//Comment inside of an annotation
+	/*@ 
+	   // This is a traditional single-line Java comment
+	   [ x == 0 -> y := 0
+	    \else x > 0 -> y := 1 
+	    \else x < 0 -> y := -1 ]
+	 @*/
+	public void commentInsideAnnotation(){}
+	
 		
 	//Java expressions
 	
+	//@ [ sum := x + y ]
+	public void sum(){
+		sum = x + y;
+	}
+	
+	//@ [ max := Math.max(x, y) ]
+	public void max(){
+		max = Math.max(x, y);
+	}
+	
+	
 	//CleanJava operators and expressions
-		//:=, &=, ->, =>
-		//anything, informal notation 
-		//else or otherwise and I 
-		//(MISSING TEST)\result - type checking
+	
+	//anything literal
+	//@ [x := \anything ]
+	public void anything(){
+		x = (int) (Math.random()*10);
+	}
+	
+	//informal notation
+	//@ [x := (* maximum of x and y *) ]
+	public void informalNotation(){
+		max = Math.max(x, y);
+	}
+	
+	//(MISSING TEST) \result - type checking
+	
 		
 	//Iterate operators
+		//(MISSING TEST) =>
 	
 	//Intended functions
-		//Concurrent assignment 
-			//(MISSING TEST)Object equality := vs &=
-		//Conditional concurrent assignment
-		//(MISSING TEST)Splitting definitions
-		//(MISSING TEST)Combining definitions
+	
+	//Simple concurrent assignment (single element)
+	//@ [ x := 3 ]
+	public void concurrentAssignment01(){
+		x = 3;
+	}
+	
+	//Simple concurrent assignment (multiple elements)
+	//@ [ x, y, z := 1, 2, 3 ]
+	public void concurrentAssignment02(){
+		x = 1;
+		y = 2;
+		z = 3;
+	}
+	
+	
+	//(MISSING TEST)Object equality := vs &=
+	
+	//Conditional concurrent assignment
+	/*@ 
+	   [ x > 0 -> y := 1
+	    \else x < 0 -> y := -1 
+	    \else \I ] //Do nothing. Original program state.
+	 @*/
+	public void conditionalConcurrentAssignment(){}
+	
+	
+	//(MISSING TEST)Splitting definitions
+	//(MISSING TEST)Combining definitions
 		
 }
